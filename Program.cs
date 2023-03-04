b82b75e0dbe12714fd093ea1f841f3f4dbc77f0c
Marcelo Bertochi <paidasamia@gmail.com>
2023-03-03 21:57:35 -0300

    Finalizando exercício das frutas

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine ("--- Minhas frutas preferidas ---");
+Console.WriteLine();
+Console.WriteLine("1. Banana");
+Console.WriteLine("2. Mamão");
+Console.WriteLine("3. Laranja");
+Console.WriteLine("4. Uva");
+Console.WriteLine("5. Morango");
+
+
